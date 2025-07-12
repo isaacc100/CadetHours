17b166b454059df5cba4bb5e3763ee65415c492d
isaacc100 <83515375+isaacc100@users.noreply.github.com>
2025-07-12 16:47:05 +0100

    1.0.0.1
    Changed main.py format for use with briefcase.

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from PySide6.QtWidgets import QApplication
 from ui_main import TimeTrackerUI
 from database import init_db
 
-if __name__ == "__main__":
+def main():
     init_db()
     app = QApplication(sys.argv)
     window = TimeTrackerUI()
     window.show()
     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
